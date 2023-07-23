0d44f9abc64dd4f3ab576dabef7372ab60fa1c02
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-07-23 07:28:19 +0200

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(cb) {
+  return cb();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    console.log("hello");
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I am an anonymous function");
+  };
+}
